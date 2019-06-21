4ff6cf107e10cfef1b769877af7225dc9d24fbce
giordifungula <jodi.fungula@gmail.com>
2019-06-21 23:46:39 +0200

    added templates and linked the js

--- microscope/client/main.js
+++ microscope/client/main.js
@@ -1,7 +1,7 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import { Template } from "meteor/templating";
+import { ReactiveVar } from "meteor/reactive-var";
 
-import './main.html';
+import "./main.html";
 
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
@@ -11,12 +11,30 @@ Template.hello.onCreated(function helloOnCreated() {
 Template.hello.helpers({
   counter() {
     return Template.instance().counter.get();
-  },
+  }
 });
 
 Template.hello.events({
-  'click button'(event, instance) {
+  "click button"(event, instance) {
     // increment the counter when button is clicked
     instance.counter.set(instance.counter.get() + 1);
+  }
+});
+
+var postsData = [
+  {
+    title: "Introducing Telescope",
+    url: "http://sachagreif.com/introducing-telescope/"
   },
+  {
+    title: "Meteor",
+    url: "http://meteor.com"
+  },
+  {
+    title: "The Meteor Book",
+    url: "http://themeteorbook.com"
+  }
+];
+Template.postsList.helpers({
+  posts: postsData
 });
